2c13eff693bd4738b31baa22dc0ca37576a67754
jmg7173 <jmg7173@gmail.com>
2018-11-20 04:16:50 +0000

    Add tag. Add apps at INSTALLED_APP in settings.py

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # our apps
+    'member',
+    'market',
+    'registration',
+    'review',
+    'tag',
+    'contract',
 ]
 
 ROOT_URLCONF = 'app.urls'
