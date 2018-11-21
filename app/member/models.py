3a43a4fc5abccd13c5eedba658e650c3012b1260
jmg7173 <jmg7173@gmail.com>
2018-11-21 17:41:47 +0900

    #1 Add member models (회원, 소비자, 사진작가)

--- app/member/models.py
+++ app/member/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+CHOICES = {
+    '회원종류': [(0, '소비자'), (1, '사진작가')]
+}
+
+
+class Member(User):
+    member_type = models.PositiveSmallIntegerField(
+        '회원종류', choices=CHOICES['회원종류'],
+    )
+    consumer_idx = models.PositiveIntegerField(
+        '소비자_idx', blank=True, null=True,
+    )
+    photographer_idx = models.PositiveIntegerField(
+        '사진작가_idx', blank=True, null=True,
+    )
+
+
+class Consumer(models.Model):
+    member_idx = models.PositiveIntegerField(
+        '회원정보_idx', blank=True, null=True,
+    )
+    contracts = ArrayField(models.PositiveIntegerField())
+
+
+class Photographer(models.Model):
+    markets = ArrayField(models.PositiveIntegerField())
+    member_idx = models.PositiveIntegerField(
+        '회원정보_idx', blank=True, null=True,
+    )
--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 
+    # local
+    'app.member',
 ]
 
 MIDDLEWARE = [
