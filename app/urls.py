cbc3fe1da78884753561526fc803c6b08146b8e7
MinGyo Jung <jmg7173@gmail.com>
2018-11-24 15:08:25 +0900

    #1 Fix miss referencing of module

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from member.views import home_view
+from app.member.views import home_view
 
 urlpatterns = [
     url(r'^home/$', home_view, name='home'),
