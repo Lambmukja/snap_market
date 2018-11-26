9f700ad3c812b28644961d3ef77b24c9820aac48
MinGyo Jung <jmg7173@gmail.com>
2018-11-26 17:35:07 +0900

    Add Django REST framework as 3rd party and also as python module

--- app/snap_web/settings.py
+++ app/snap_web/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
     # local
     'snap_web',
     'member',
