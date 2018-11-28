19ddeab329e5aff80faffa90780f98e6469a5d9b
MinGyo Jung <jmg7173@gmail.com>
2018-11-28 17:38:13 +0900

    #10 Move home view to snap_web/views.py and Add logout view
    Also added base.html for base template, base css(style.css), Fix home url to '/'.
    TODO: @ddyo page header 버튼 오른쪽으로 가게 옮겨주고 login누르면 화면이 안나옴 ㅠㅠ

--- app/member/urls.py
+++ app/member/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from member.views import signup_view, login_view
+from member.views import signup_view, login_view, logout_view
 
 urlpatterns = [
     path('signup/', signup_view, name='member_signup'),
     path('login/', login_view, name='member_login'),
+    path('logout/', logout_view, name='member_logout'),
 ]
