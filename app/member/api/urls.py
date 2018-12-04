3ca67ea032806f45fa8d39678000912b5fb322f4
MinGyo Jung <jmg7173@gmail.com>
2018-12-04 15:28:09 +0900

    #5 Add views at urls (views were missed)

--- app/member/api/urls.py
+++ app/member/api/urls.py
@@ -4,7 +4,7 @@ from member.api import views
 
 urlpatterns = [
     path('photographers/', views.PhotographerListView.as_view(), name='api_member_photographer_list'),
-    path('photographers/<int:pk>/', name='api_member_photographer_detail'),
-    path('consumers/', name='api_member_consumer_list'),
-    path('consumers/<int:pk>', name='api_member_consumer_detail'),
+    path('photographers/<int:pk>/', views.PhotographerDetailView.as_view(), name='api_member_photographer_detail'),
+    path('consumers/', views.ConsumerListView.as_view(), name='api_member_consumer_list'),
+    path('consumers/<int:pk>', views.ConsumerDetailView, name='api_member_consumer_detail'),
 ]
