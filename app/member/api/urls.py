1c92abb1e45d186bdec9b84cba38551ca5417ef4
MinGyo Jung <jmg7173@gmail.com>
2018-12-05 17:05:32 +0900

    #5 Fix api view bug

--- app/member/api/urls.py
+++ app/member/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('photographers/', views.PhotographerListView.as_view(), name='api_member_photographer_list'),
     path('photographers/<int:pk>/', views.PhotographerDetailView.as_view(), name='api_member_photographer_detail'),
     path('consumers/', views.ConsumerListView.as_view(), name='api_member_consumer_list'),
-    path('consumers/<int:pk>', views.ConsumerDetailView, name='api_member_consumer_detail'),
+    path('consumers/<int:pk>/', views.ConsumerDetailView.as_view(), name='api_member_consumer_detail'),
 ]
