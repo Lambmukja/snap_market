097e67fee985e33bc778c0e80765625745b46591
MinGyo Jung <jmg7173@gmail.com>
2018-12-08 12:38:47 +0900

    #27 Add tags at market article page

--- app/market/views.py
+++ app/market/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from market.models import Market
+from tag.models import Tag
 
 
 def market_post_view(request, pk):
     market = Market.objects.get(pk=pk)
-    context = {"market": market}
+    tags = Tag.objects.filter(pk__in=market.tags)
+    context = {"market": market, "tags": tags}
     return render(request, "market/post.html", context)
