fc393cc2489133a8e2d3a13c84c7ceec1e03f506
MinGyo Jung <jmg7173@gmail.com>
2018-12-08 15:56:38 +0900

    #27 Add tags at main page and add padding

--- app/snap_web/views.py
+++ app/snap_web/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 from market.models import Market
+from tag.models import Tag
 
 
 def home_view(request):
     if request.method == 'GET':
         markets = Market.objects.all()
+        tags = Tag.objects.all()
         posts = []
         for market in markets:
             posts.append({
@@ -13,5 +15,5 @@ def home_view(request):
                 'market_name': market.studio_name,
                 'market_post': market.posts,
             })
-        context = {'posts': posts}
+        context = {'posts': posts, 'tags': tags}
         return render(request, "home.html", context)
