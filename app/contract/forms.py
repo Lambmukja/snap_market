97ac0635a35c14c5279a70e79e95ff1a8bcf18bf
MinGyo Jung <jmg7173@gmail.com>
2018-12-13 22:22:53 +0900

    #34 Add specific datetime field for contract form

--- app/contract/forms.py
+++ app/contract/forms.py
@@ -4,6 +4,17 @@ from contract.models import Contract
 
 
 class ContractForm(forms.ModelForm):
+    start_time = forms.DateTimeField(label='시작시간',
+                                     widget=forms.SplitDateTimeWidget(
+                                         date_format='%Y-%m-%d',
+                                         time_format='%H:%M',
+                                     ))
+    end_time = forms.DateTimeField(label='종료시간',
+                                   widget=forms.SplitDateTimeWidget(
+                                       date_format='%Y-%m-%d',
+                                       time_format='%H:%M',
+                                   ))
+
     class Meta:
         model = Contract
         fields = ("consumer_idx", "market_idx", "start_time", "end_time")
