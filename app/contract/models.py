7b0c5c010e9ab3198ba99296f095d98a8df5d1d6
MinGyo Jung <jmg7173@gmail.com>
2018-12-13 23:43:25 +0900

    #34 Add cost field at contract

--- app/contract/models.py
+++ app/contract/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Contract(models.Model):
     market_idx = models.PositiveSmallIntegerField(blank=False, null=False)
     consumer_idx = models.PositiveSmallIntegerField(blank=False, null=False)
+    cost = models.PositiveIntegerField(blank=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
