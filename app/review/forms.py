e5fe970dd672bb8ddc133448cb361ab09a75cd49
MinGyo Jung <jmg7173@gmail.com>
2018-12-23 21:27:49 +0900

    #50 Add customizing at stars.

--- app/review/forms.py
+++ app/review/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
+import review
 from review.models import Review
 
 
 class ReviewForm(forms.ModelForm):
+    stars = forms.ChoiceField(
+        choices=review.models.CHOICES['별점'],
+    )
+
     class Meta:
         model = Review
         fields = ('stars', 'review')
