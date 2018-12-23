cd9469e50a26eb6e122307d19d3e693cbcf926c7
MinGyo Jung <jmg7173@gmail.com>
2018-12-23 14:35:31 +0900

    #31 Delete debugging print

--- app/member/views.py
+++ app/member/views.py
@@ -75,6 +75,5 @@ def mypage_view(request):
         context['contracts'] = {}
         for market in markets:
             context['contracts'][market.studio_name] = Contract.objects.filter(pk__in=market.contract_idxs)
-        print(context['contracts'])
 
     return render(request, "member/mypage.html", context)
