c0cea7fc4d8d025b1fdc0893b54c527d6625597e
Dohyun Kim <dhdh6190@gmail.com>
2018-12-23 22:03:05 +0900

    #50 Edit Review page Ui

--- app/review/forms.py
+++ app/review/forms.py
@@ -7,6 +7,7 @@ from review.models import Review
 class ReviewForm(forms.ModelForm):
     stars = forms.ChoiceField(
         choices=review.models.CHOICES['별점'],
+        widget=forms.Select(attrs={'class':'custom-select'})
     )
 
     class Meta:
