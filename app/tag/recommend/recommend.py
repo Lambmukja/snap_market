2fb22ea280d6c99b47f89a2539d0b8a14bb7a38d
MinGyo Jung <jmg7173@gmail.com>
2018-12-23 23:38:02 +0900

    #60 Fix recommendation
    Input tag is tag id. But in function, it is using tag_type field.
    Also, it returns market as increasing order. But, we need decreasing order market

--- app/tag/recommend/recommend.py
+++ app/tag/recommend/recommend.py
@@ -1,6 +1,6 @@
 import statistics
 from operator import itemgetter
-from typing import List, Tuple
+from typing import List
 
 from market.models import Market
 from tag.models import Tag
@@ -16,23 +16,26 @@ def recommend(user_tags: List[int]):
     """
     tags = Tag.objects.all()
     markets = Market.objects.all()
-    tag_types = set(user_tags)
+    user_tag_ids = set(user_tags)
     result = list()
-
     for market in markets:
         # get only market's tags and convert to set
         market_tags = set(market.tags)
-        matched_tags = list(market_tags & tag_types) # set & operation
+        matched_tags = list(market_tags & user_tag_ids)  # set & operation
+
         # calculate average matched weight
-        filtered_tags = Tag.objects.filter(tag_type__in=matched_tags)
-        filtered_tag_weights = [tags[tag.tag_type].weight for tag in filtered_tags]
+        filtered_tags = Tag.objects.filter(id__in=matched_tags)
+        filtered_tag_weights = [tags[tag.id].weight for tag in filtered_tags]
+
         # if no matched tag
         if not filtered_tags:
             continue
         avg_weight = statistics.mean(filtered_tag_weights)
+
         # calculate match_rate
         match_rate = len(matched_tags) / len(user_tags)
         score = Config.ALPHA * avg_weight + Config.GAMMA * match_rate
-        result.append((market.studio_name, score))
-    result = sorted(result, key=itemgetter(1))  # sort with score
+        result.append((market, score))
+
+    result = sorted(result, key=itemgetter(1), reverse=True)  # sort with as decreasing order
     return result
